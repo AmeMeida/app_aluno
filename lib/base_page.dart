028935e29a23b3623c4f792277a24b22b236b944
AmeMeida <ame.pistache@gmail.com>
2023-09-12 11:17:18 -0300

    primo commit

---
+++ lib/base_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Layout extends Scaffold {
+  final String title;
+  final Widget content;
+
+  Layout(BuildContext context, {super.key, required this.title, required this.content}) : super(
+    appBar: AppBar(
+      title: Text(title),
+    ),
+    body: content,
+    bottomNavigationBar: BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          label: "Home",
+          icon: IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () => Navigator.of(context).pushNamed("/"),
+          )),
+        BottomNavigationBarItem(
+          label: "Cadastro",
+          icon: IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () => Navigator.of(context).pushNamed("/cadastro")
+          )),
+        BottomNavigationBarItem(
+          label: "Consulta",
+          icon: IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () => Navigator.of(context).pushNamed("/consulta"),
+          )),
+        BottomNavigationBarItem(
+          label: "Consulta",
+          icon: IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () => Navigator.of(context).pushNamed("/lista"),
+          ))
+      ])
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:cadastro_aluno/pages/cadastro.dart';
+import 'package:cadastro_aluno/pages/consulta.dart';
+import 'package:cadastro_aluno/pages/home.dart';
+import 'package:cadastro_aluno/pages/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cadastro de Alunos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        "/": (context) => const HomePage(),
+        "/cadastro": (context) => const CadastroPage(),
+        "/consulta": (context) => const ConsultaPage(),
+        "/lista": (context) => const ListaPage()
+      },
+    );
+  }
+}
